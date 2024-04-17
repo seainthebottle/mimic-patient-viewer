ef77243c9d2a552e86ee163060bd8154849bb5bc
seainthebottle <seainthebottle@gmail.com>
2024-04-17 16:21:51 +0900

    간단 바이탈 시트 구현

--- src/vital_sheet/vital_sheet_widget.py
+++ src/vital_sheet/vital_sheet_widget.py
@@ -52,20 +52,19 @@ class VitalSheetWidget(QtWidgets.QWidget):
             self.ax3.clear()  # Also clear the third axis
 
             # 하루 동안의 모든 시간을 포함하는 Timestamp 생성
-            # Define the date range without 'closed'
             full_day_range = pd.date_range(start=pd.to_datetime(self.current_date), end=pd.to_datetime(self.current_date) + pd.Timedelta(days=1), freq='h')
-            # Manually handle the range to simulate 'closed' if needed
-            full_day_range = full_day_range[:-1]  # Exclude the last hour if mimicking 'closed='left''
+            #full_day_range = pd.date_range(start=pd.to_datetime(self.current_date), end=pd.to_datetime(self.current_date) + pd.Timedelta(hours=25), freq='h')
+            #full_day_range = full_day_range[:-1]  # Exclude the last hour if mimicking 'closed='left''
 
             mask = (self.dataFrame['timestamp'].dt.date == date.date()) | (self.dataFrame['timestamp'] == date + pd.Timedelta(days=1))
             daily_data = self.dataFrame[mask]
+            daily_data = daily_data.fillna(0)  # 누락된 데이터는 0으로 채움
+            #daily_data['input_ml'] = daily_data['input_ml'].fillna(0).round(0).astype(int)
+            #daily_data['output_ml'] = daily_data['output_ml'].fillna(0).round(0).astype(int)
 
             # 전체 시간대로 인덱싱
             daily_data.set_index('timestamp', inplace=True)
             daily_data = daily_data.reindex(full_day_range)
-            daily_data['input_ml'] = daily_data['input_ml'].fillna(0).round(0).astype(int)
-            daily_data['output_ml'] = daily_data['output_ml'].fillna(0).round(0).astype(int)
-            daily_data = daily_data.fillna(0)  # 누락된 데이터는 0으로 채움
             daily_data.reset_index(inplace=True)
             daily_data.rename(columns={'index': 'timestamp'}, inplace=True)
 
@@ -77,11 +76,17 @@ class VitalSheetWidget(QtWidgets.QWidget):
                 bt_data = daily_data[daily_data['BT'] != 0]
 
                 # Plot blood pressure
+
+                #print(sbp_data, dbp_data)
                 self.ax1.plot(sbp_data['timestamp'], sbp_data['NBPs'], label='SBP', marker='o', linestyle='--', color='red')
                 self.ax1.plot(dbp_data['timestamp'], dbp_data['NBPd'], label='DBP', marker='o', linestyle='--', color='blue')
                 self.ax1.fill_between(gbp_data['timestamp'], gbp_data['NBPd'], gbp_data['NBPs'], color='grey', alpha=0.3, label='Pressure Gap')
                 self.ax1.set_title('Blood Pressure')
                 self.ax1.legend(loc='upper right')
+                #max_sbp = sbp_data['NBPs'].max()  
+                #min_dbp = dbp_data['NBPd'].min() 
+                #print(sbp_data, dbp_data)
+                #if max_sbp and min_dbp: self.ax1.set_ylim(min_dbp, max_sbp)
                 self.ax1.grid(True)
 
                 # Plot heart rate
@@ -90,14 +95,14 @@ class VitalSheetWidget(QtWidgets.QWidget):
                 #self.ax2.legend(loc='upper right')
                 self.ax2.grid(True)
                 # Set the lower limit of heart rate to 0 and determine a suitable upper limit
-                max_heart_rate = daily_data['HR'].max() + 10  # Adding a buffer for better visibility
-                self.ax2.set_ylim(0, max_heart_rate)
+                max_heart_rate = hr_data['HR'].max() + 50  # Adding a buffer for better visibility
+                if not pd.isna(max_heart_rate): self.ax2.set_ylim(30, max_heart_rate)
 
                 # Plot body temperature on a separate axis
                 self.ax3.plot(bt_data['timestamp'], bt_data['BT'].replace(0, None), label='Body Temperature', marker='o', linestyle='-', color='orange')
                 #self.ax3.legend(loc='upper right')
                 self.ax3.tick_params(axis='y', labelcolor='orange')
-                self.ax3.set_ylim(30, 43)  # Set the limits for body temperature
+                self.ax3.set_ylim(33, 45)  # Set the limits for body temperature
 
                 # Collect legend handles and labels from both ax2 and ax3
                 handles, labels = [], []
@@ -117,11 +122,13 @@ class VitalSheetWidget(QtWidgets.QWidget):
                     ax.xaxis.set_major_formatter(mdates.DateFormatter('%H'))
                     ax.set_xlim([pd.to_datetime(date), pd.to_datetime(date) + pd.Timedelta(days=1)])
 
+            if not daily_data.empty:
                 # Add table below the second graph
                 table_data = daily_data[['timestamp', 'input_ml', 'output_ml']].copy()
+                table_data = table_data[:-1]
                 table_data['timestamp'] = table_data['timestamp'].dt.strftime('%H:%M')
-                #table_data['input_ml'] = table_data['input_ml'].round(0).astype(int)
-                #table_data['output_ml'] = table_data['output_ml'].round(0).astype(int)
+                table_data['input_ml'] = table_data['input_ml'].apply(lambda x: '' if pd.isna(x) else int(x))
+                table_data['output_ml'] = table_data['output_ml'].apply(lambda x: '' if pd.isna(x) else int(x))
                 transposed_data = table_data.T
                 cell_text = transposed_data.values.tolist()
                 table = self.ax2.table(cellText=cell_text, rowLabels=transposed_data.index, loc='bottom', bbox=[0, -0.45, 1, 0.3], fontsize=9)
