1c1c6f8307998c6f7137a3f5482dc243db191880
seainthebottle <seainthebottle@gmail.com>
2024-05-03 13:36:55 +0900

    인덱스 추가

--- queries/additional_queries.sql
+++ queries/additional_queries.sql
@@ -1,2 +1,4 @@
 CREATE INDEX chartevents_idxs1 ON mimiciv_icu.chartevents(hadm_id)
+CREATE INDEX chartevents_idxs2 on mimiciv_icu.chartevents(subject_id)
 CREATE INDEX labevents_idxs1 ON mimiciv_hosp.labevents(hadm_id)
+CREATE INDEX outputevents_idxs1 on mimiciv_icu.outputevents(subject_id)
