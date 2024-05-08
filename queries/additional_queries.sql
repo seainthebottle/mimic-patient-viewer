447130ccc59cc46146664f05f148a8f36c8d0466
seainthebottle <seainthebottle@gmail.com>
2024-05-08 21:34:50 +0900

    인덱스를 늘림

--- queries/additional_queries.sql
+++ queries/additional_queries.sql
@@ -1,4 +1,6 @@
 CREATE INDEX chartevents_idxs1 ON mimiciv_icu.chartevents(hadm_id)
 CREATE INDEX chartevents_idxs2 on mimiciv_icu.chartevents(subject_id)
+CREATE INDEX chartevents_idxs3 on mimiciv_icu.chartevents(stay_id)
 CREATE INDEX labevents_idxs1 ON mimiciv_hosp.labevents(hadm_id)
+CREATE INDEX labevents_idxs2 ON mimiciv_hosp.labevents(subject_id)
 CREATE INDEX outputevents_idxs1 on mimiciv_icu.outputevents(subject_id)
