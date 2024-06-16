2acda8d2f5af9a34a11af0cd151c172b0df963a5
seainthebottle <seainthebottle@gmail.com>
2024-06-16 18:42:30 +0900

    Add indexes

--- queries/additional_queries.sql
+++ queries/additional_queries.sql
@@ -1,8 +1,20 @@
+CREATE INDEX admission_idxs1 ON mimiciv_hosp.admissions(subject_id)
+
+CREATE INDEX labevents_idxs1 ON mimiciv_hosp.labevents(hadm_id)
+CREATE INDEX labevents_idxs2 ON mimiciv_hosp.labevents(subject_id)
+CREATE INDEX labevents_idxs3 ON mimiciv_hosp.labevents(charttime)
+CREATE INDEX labevents_idxs4 ON mimiciv_hosp.labevents(itemid)
+
+CREATE INDEX emar_idxs1 on mimiciv_hosp.emar(hadm_id)
+
 CREATE INDEX chartevents_idxs1 ON mimiciv_icu.chartevents(hadm_id)
 CREATE INDEX chartevents_idxs2 on mimiciv_icu.chartevents(subject_id)
 CREATE INDEX chartevents_idxs3 on mimiciv_icu.chartevents(stay_id)
 CREATE INDEX chartevents_idxs4 on mimiciv_icu.chartevents(charttime)
-CREATE INDEX labevents_idxs1 ON mimiciv_hosp.labevents(hadm_id)
-CREATE INDEX labevents_idxs2 ON mimiciv_hosp.labevents(subject_id)
+CREATE INDEX chartevents_idxs5 on mimiciv_icu.chartevents(itemid)
+
 CREATE INDEX outputevents_idxs1 on mimiciv_icu.outputevents(subject_id)
-CREATE INDEX outputevents_idxs2 on mimiciv_icu.chartevents(charttime)
+CREATE INDEX outputevents_idxs2 on mimiciv_icu.outputevents(charttime)
+CREATE INDEX outputevents_idxs3 on mimiciv_icu.outputevents(stay_id)
+
+CREATE INDEX discharge_idxs1 on mimiciv_note.discharge(hadm_id)
