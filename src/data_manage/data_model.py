aae6164636abbd96739155813d66d4d08726f798
seainthebottle <seainthebottle@gmail.com>
2024-12-12 13:19:22 +0900

    프로시저 출력을 seq_num 대로

--- src/data_manage/data_model.py
+++ src/data_manage/data_model.py
@@ -203,7 +203,7 @@ class DataModel:
         JOIN mimiciv_hosp.d_icd_procedures dicd 
           ON pr.icd_code = dicd.icd_code AND pr.icd_version = dicd.icd_version
         WHERE pr.hadm_id = %s
-        ORDER BY pr.chartdate
+        ORDER BY pr.seq_num
         """
         try:
             self.cursor.execute(query, (hadm_id,))
